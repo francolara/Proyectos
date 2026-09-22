3a82caab80367aff61de6dc3872b68190b7af492
Franco Lara <luifranco20@gmail.com>
2026-09-22 14:33:05 -0500

    Diseño Web Fralsecont
    Diseño Web Fralsecont

--- .NET2026/SistemaAdministrativoWeb/obj/Debug/net10.0/SistemaAdministrativoWeb.AssemblyInfo.cs
+++ .NET2026/SistemaAdministrativoWeb/obj/Debug/net10.0/SistemaAdministrativoWeb.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaAdministrativoWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31f06f8a2b632dfbb8142a9594e6438bf133b5b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e97434890700081b74fb9b543ff4c15ec0ce300b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaAdministrativoWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaAdministrativoWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
